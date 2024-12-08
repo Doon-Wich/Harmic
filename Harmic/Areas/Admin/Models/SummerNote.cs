55d50b2e1f756024e29c9857cb9799c331a649d3
PhanTuan1709 <phantuan20012018@gmail.com>
2024-12-08 11:33:02 +0700

    manage image

---
+++ Harmic/Areas/Admin/Models/SummerNote.cs
@@ -0,0 +1,28 @@
+﻿namespace Harmic.Areas.Admin.Models
+{
+    public class SummerNote
+    {
+        public SummerNote(string idEditor, bool loadLibary = true)
+        {
+
+            IDEditor = idEditor;
+            LoadLibrary = loadLibary;
+
+        }
+        public string IDEditor { get; set; }
+        public bool LoadLibrary { get; set; }
+        public int Height { get; set; } = 500;
+        public string toolBar { get; set; } = @"
+            [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ol', 'ul', 'paragraph']],
+                ['table',['table']],
+                ['insert', ['link', 'elfinderFile', 'video', 'elfinder']],
+                ['view', ['fullscreen', 'codeview', 'help']],
+                
+            ]
+        ";
+    }
+}
